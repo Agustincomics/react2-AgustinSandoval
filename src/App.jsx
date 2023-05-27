aced42b8e53ebdff44ea5f29f8a73f45d3b6c50b
Agustin Sandoval <94272906+Agustincomics@users.noreply.github.com>
2023-05-26 23:19:10 -0300

    cambios finales

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import HolaMundo from './components/holaMundo.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <HolaMundo name="My friend"/>
+      </div>
+    </>
+  )
+}
+
+
+
+export default App
